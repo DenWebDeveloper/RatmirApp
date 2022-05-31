9721f0b5de9b969804671e0bac36c71c0c5c7f04
Denis Shabanov <step-klim@mail.ru>
2022-05-31 21:17:20 +0300

    Добавил Всплытие Окна с извинением!

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
 window.onload = function () {
 
+    // Находим элементы на странице сайта
+    const body = document.querySelector('body');
     const section1 = document.querySelector('.section-1');
-
     const btnBurger = document.querySelector('.btn-burger');
     const btnClouse = document.querySelector('.btn-clouse');
     const burgerMenu = document.querySelector('.menu-burger');
+
+    const windowError = document.querySelector('.wrapper__window-error ');
+
+    const windowBtnClose = document.querySelector('.wrapper__btn-exit');
+
+    const socialLinksIcons = document.querySelectorAll(".item__social-icon ");
+
+    console.log(windowBtnClose);
+
+    socialLinksIcons.forEach((item) => {
+
+        item.addEventListener('click', () => {
+            windowError.classList.add('active');
+            body.classList.add('disabled');
+        });
+
+        windowBtnClose.addEventListener('click', () => {
+            windowError.classList.remove('active');
+            body.classList.remove('disabled');
+
+
+        });
+
+    });
+
+
 
 
     btnBurger.addEventListener('click', (e) => {
