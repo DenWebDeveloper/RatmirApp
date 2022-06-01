6d07f730170eaae0972e56b1be5c7b26469890d8
Denis Shabanov <step-klim@mail.ru>
2022-06-01 14:07:24 +0300

    Доделал Галерею

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@ window.onload = function () {
 
     // Находим элементы на странице сайта
     const body = document.querySelector('body');
-    const section1 = document.querySelector('.section-1');
+    const section1 = document.querySelector('.sec-1');
+
+    const section2 = document.querySelector('.sec-2');
+
     const btnBurger = document.querySelector('.btn-burger');
     const btnClouse = document.querySelector('.btn-clouse');
     const burgerMenu = document.querySelector('.menu-burger');
@@ -13,7 +16,8 @@ window.onload = function () {
 
     const socialLinksIcons = document.querySelectorAll(".item__social-icon ");
 
-    console.log(windowBtnClose);
+    const itemMobileMeu = document.querySelectorAll('.item__mobile-menu');
+
 
     socialLinksIcons.forEach((item) => {
 
@@ -32,13 +36,13 @@ window.onload = function () {
     });
 
 
-
-
-    btnBurger.addEventListener('click', (e) => {
+    btnBurger.addEventListener('click', () => {
 
         btnBurger.classList.remove('active');
         burgerMenu.classList.add('active');
         btnClouse.classList.add('active');
+        body.classList.add('disabled');
+
 
         
     });
@@ -47,11 +51,10 @@ window.onload = function () {
 
         btnClouse.classList.remove('active');
         burgerMenu.classList.remove('active');
-
-
+        body.classList.remove('disabled');
         btnBurger.classList.add('active');
       
     });
-
+    
 
 };
